eb7631be870e677b69a63bc9d156b88275fb5c29
Jamie Pittak <charrawr456@gmail.com>
2018-03-19 19:08:13 -0700

    add test and functions for age on planets

--- js/galactic-age.js
+++ js/galactic-age.js
@@ -5,11 +5,40 @@ export class GalacticAge {
     this.lifeExpectancy = lifeExpectancy;
   }
 
-ageInSeconds() {
-  let userBirthDate = new Date(this.birthDate)
-  let todayDate = new Date();
-  return Math.floor((todayDate.getTime() - userBirthDate.getTime()) / 1000);
-}
+  ageInSeconds() {
+    let userBirthDate = new Date(this.birthDate)
+    let todayDate = new Date();
+    return Math.floor((todayDate.getTime() - userBirthDate.getTime()) / 1000);
+  }
+
+  ageOnEarth(){
+    let ageOnEarth = Math.floor(this.ageInSeconds() / 31536000);
+    return ageOnEarth;
+  }
+
+  ageOnMercury(){
+    let ageOnMercury = Math.floor((this.ageInSeconds() / 31536000) / .24);
+    return ageOnMercury;
+  }
+
+  ageOnVenus(){
+    let ageOnVenus = Math.floor((this.ageInSeconds() / 31536000) / .62);
+    return ageOnVenus;
+  }
+
+  ageOnMars(){
+    let ageOnMars = Math.floor((this.ageInSeconds() / 31536000) / 1.88);
+    return ageOnMars;
+  }
+
+  ageOnJupiter(){
+    let ageOnJupiter = Math.floor((this.ageInSeconds() / 31536000) / 11.86);
+    return ageOnJupiter;
+  }
+
+  lifeExpectancy(){
+    let planetLifeExpectancy = 0;
 
 
+  }
 }
--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -21,6 +21,38 @@ describe('ageOnMercury', function(){
   it ('should test age on Mercury is correct', function(){
     let userInput = new GalacticAge('1990-03-30', 85);
 
-    expect().toEqual();
+    expect(userInput.ageOnMercury()).toEqual(116);
+  });
+});
+
+describe('ageOnEarth', function(){
+  it ('should test age on Earth is correct', function(){
+    let userInput = new GalacticAge('1990-03-30', 85);
+
+    expect(userInput.ageOnEarth()).toEqual(27);
+  });
+});
+
+describe('ageOnVenus', function(){
+  it ('should test age on Venus is correct', function(){
+    let userInput = new GalacticAge('1990-03-30', 85);
+
+    expect(userInput.ageOnVenus()).toEqual(45);
+  });
+});
+
+describe('ageOnMars', function(){
+  it ('should test age on Mars is correct', function(){
+    let userInput = new GalacticAge('1990-03-30', 85);
+
+    expect(userInput.ageOnMars()).toEqual(14);
+  });
+});
+
+describe('ageOnJupiter', function(){
+  it ('should test age on Jupiter is correct', function(){
+    let userInput = new GalacticAge('1990-03-30', 85);
+
+    expect(userInput.ageOnJupiter()).toEqual(2);
   });
 });
