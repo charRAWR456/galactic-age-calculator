05a62c9c50241845d1707945d1e916d458a0bfa4
Jamie Pittak <charrawr456@gmail.com>
2018-03-19 19:43:52 -0700

    update ui to show years for each planet with jquery

--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+import {GalacticAge} from './../js/galactic-age.js';
+
+$(document).ready(function() {
+  $("#user-info").submit(function(event){
+    event.preventDefault();
+
+    let birthday = $("#user-birthday").val();
+    let lifeExpectancy = $("#user-life-expectancy").val();
+console.log(birthday);
+console.log(lifeExpectancy);
+    let newUser = new GalacticAge(birthday, lifeExpectancy);
+    $("#results").show();
+    console.log(newUser.ageOnEarth());
+    $("#mercury").text(newUser.ageOnMercury());
+    $("#venus").text(newUser.ageOnVenus());
+    $("#earth").text(newUser.ageOnEarth());
+    $("#mars").text(newUser.ageOnMars());
+    $("#jupiter").text(newUser.ageOnJupiter());
+
+
+  });
+});
